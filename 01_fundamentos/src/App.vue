d6be28237feb36990e5f9411f9b210c3334bab2e
Cristhian David Recalde <89264838+Isnotcristhianr@users.noreply.github.com>
2024-04-06 09:30:02 -0500

    Reestructurar componentes en App.vue y TheWelcome.vue

--- 01_fundamentos/src/App.vue
+++ 01_fundamentos/src/App.vue
@@ -1,12 +1,43 @@
 <template>
-  <div style="align-items: center; text-align: center;">
+  <div class="container">
     <img alt="Vue logo" src="./assets/logo.svg" width="125" height="125">
-    <h1>Hola VueJs 3</h1>
-    <hr>
-    <h2>Contador</h2>
+    <small>
+      <h1>Hola VueJs 3</h1>
+    </small>
+    <hr/>
+    <h2>Contador: </h2>
     <button>+1</button>
     <button>-1</button>
     <button>Reset</button>
   </div>
 
 </template>
+
+<style scoped>
+  .container {
+    text-align: center;
+  }
+
+  h1{
+    font-weight: bolder;
+  }
+
+  small{
+    color: #666;
+  }
+
+  h2{
+    font-size: 2.5rem;
+  }
+
+  button{
+    cursor: pointer;
+    font-size: 1.5rem;
+    padding: 0.5rem 1rem;
+    margin: 0 0.5rem;
+    border-radius: 0.5rem;
+    background-color: #41b883;
+    color: white;
+    border: none;
+  }
+</style>
