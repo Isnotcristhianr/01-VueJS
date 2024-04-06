cf2be05bf48ba71d4fccc484fd7e37db3b98c94a
Cristhian David Recalde <89264838+Isnotcristhianr@users.noreply.github.com>
2024-04-06 10:13:55 -0500

    Reestructurar componentes en App.vue y TheWelcome.vue, agregar funcionalidad de contador

--- 01_fundamentos/src/App.vue
+++ 01_fundamentos/src/App.vue
@@ -1,43 +1,63 @@
+<script>
+export default {
+  data: () => ({
+    contador: 100
+  }),
+  methods: {
+    incrementar() {
+      this.contador++;
+    },
+    decrementar() {
+      this.contador--;
+    },
+    reset() {
+      this.contador = 0;
+    }
+  }
+};
+</script>
+
+
 <template>
   <div class="container">
     <img alt="Vue logo" src="./assets/logo.svg" width="125" height="125">
     <small>
       <h1>Hola VueJs 3</h1>
     </small>
-    <hr/>
-    <h2>Contador: </h2>
-    <button>+1</button>
-    <button>-1</button>
-    <button>Reset</button>
+    <hr />
+    <h2>Contador: {{ contador }}</h2>
+    <button @click=" incrementar()">+1</button>
+    <button @click="decrementar()">-1</button>
+    <button @click="reset()">Reset</button>
   </div>
 
 </template>
 
 <style scoped>
-  .container {
-    text-align: center;
-  }
+.container {
+  text-align: center;
+}
 
-  h1{
-    font-weight: bolder;
-  }
+h1 {
+  font-weight: bolder;
+}
 
-  small{
-    color: #666;
-  }
+small {
+  color: #666;
+}
 
-  h2{
-    font-size: 2.5rem;
-  }
+h2 {
+  font-size: 2.5rem;
+}
 
-  button{
-    cursor: pointer;
-    font-size: 1.5rem;
-    padding: 0.5rem 1rem;
-    margin: 0 0.5rem;
-    border-radius: 0.5rem;
-    background-color: #41b883;
-    color: white;
-    border: none;
-  }
+button {
+  cursor: pointer;
+  font-size: 1.5rem;
+  padding: 0.5rem 1rem;
+  margin: 0 0.5rem;
+  border-radius: 0.5rem;
+  background-color: #41b883;
+  color: white;
+  border: none;
+}
 </style>
