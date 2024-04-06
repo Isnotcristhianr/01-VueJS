49fd3cdaefbfeea85d102bde7f4b3ba17d28f758
Cristhian David Recalde <89264838+Isnotcristhianr@users.noreply.github.com>
2024-04-05 23:03:08 -0500

    Reestructurar componentes en App.vue y TheWelcome.vue

--- 01_fundamentos/src/App.vue
+++ 01_fundamentos/src/App.vue
@@ -1,49 +1,12 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <div>
-    <header>
-      <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-      <div class="wrapper">
-        <HelloWorld msg="You did it!" />
-      </div>
-    </header>
-
-    <main>
-      <TheWelcome />
-    </main>
+  <div style="align-items: center; text-align: center;">
+    <img alt="Vue logo" src="./assets/logo.svg" width="125" height="125">
+    <h1>Hola VueJs 3</h1>
+    <hr>
+    <h2>Contador</h2>
+    <button>+1</button>
+    <button>-1</button>
+    <button>Reset</button>
   </div>
+
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
