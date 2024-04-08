820be846637b2371afad03060218d4104b460791
Cristhian David Recalde <89264838+Isnotcristhianr@users.noreply.github.com>
2024-04-07 22:41:09 -0500

    Eliminar archivos y estilos no utilizados en el formulario

--- 04_formularios/src/App.vue
+++ 04_formularios/src/App.vue
@@ -1,47 +1,90 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+export default {
+  data() {
+    return {
+      url: "https://cdn.svgporn.com/logos/vue.svg?response-content-disposition=attachment%3Bfilename%3Dvue.svg"
+    };
+  },
+  methods: {
+    addUser() {
+      console.log('Usuario agregado')
+    }
+  }
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="dir">
+    <h1>Formularios</h1>
+    <img :src="url" alt="Vue" width="150">
+    <hr>
+    <!-- Formulario -->
+    <form @submit.prevent="addUser">
+      <div>
+        <label>Nombre
+          <input type="text" placeholder="Nombre" required>
+        </label>
+      </div>
+      <br>
+      <div>
+        <label>Edad
+          <input type="number" placeholder="Edad" required>
+        </label>
+      </div>
+      <br>
+      <div>
+        <label>Correo
+          <input type="email" placeholder="Correo" required>
+        </label>
+      </div>
+      <br>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+      <button type="submit">Enviar</button>
 
-  <main>
-    <TheWelcome />
-  </main>
+    </form>
+  </div>
 </template>
 
+
+
 <style scoped>
-header {
-  line-height: 1.5;
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
+
+.dir {
+  font-family: 'Roboto', sans-serif;
+  text-align: center;
+  background-color: #f1f1f1;
+  padding: 20px;
+  border-radius: 5px;
+  margin: 20px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+label {
+  margin: 10px;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+input {
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+button {
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  background-color: #333;
+  color: #fff;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #555;
 }
 </style>
