f01896bc64e7a4bb5670e79856c61ee642175c68
Cristhian David Recalde <89264838+Isnotcristhianr@users.noreply.github.com>
2024-04-29 22:44:12 -0500

    Actualizar formulario con nuevos campos y validaciones

--- 04_formularios/src/assets/components/Formulario.vue
+++ 04_formularios/src/assets/components/Formulario.vue
@@ -5,16 +5,26 @@
       <div class="col-12">
         <form @submit.prevent="">
           <div class="form-group">
-            <label for="nombre">Nombre</label>
-            <input type="text" class="form-control" id="nombre" >
+            <label for="nombre">Proyecto</label>
+            <input type="text" class="form-control" id="nombre" required>
           </div>
+          <br>
           <div class="form-group">
-            <label for="edad">Edad</label>
-            <input type="number" class="form-control" id="edad" >
+            <label for="form-label">Actividad</label>
+            <select name="" id="" class="form-select text-center " required>
+              <option value="" disabled>
+                Seleccione una actividad
+              </option>
+              <option value="1" >Desarrollo</option>
+              <option value="2">Diseño</option>
+              <option value="3">Administración</option>
+            </select>
           </div>
-          <div class="form-group">
-            <label for="correo">Correo</label>
-            <input type="email" class="form-control" id="correo" >
+          <br>
+          <div class="mb-3">
+            <label for="" class="form-check-label">
+              <input type="checkbox" class="form-check-input" id="terminos"> Acepto términos y condiciones
+            </label>
           </div>
           <button type="submit" class="btn btn-success m-2">Enviar</button>
         </form>
