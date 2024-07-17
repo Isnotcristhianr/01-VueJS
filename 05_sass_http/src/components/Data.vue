e6a27be0e5873a88b88d2d02fc0ad40ea1e259af
Cristhian David Recalde <89264838+Isnotcristhianr@users.noreply.github.com>
2024-07-16 23:33:03 -0500

    diseño de la tabla y mejoras

--- 05_sass_http/src/components/Data.vue
+++ 05_sass_http/src/components/Data.vue
@@ -1,6 +1,20 @@
 <template>
-    <div>
-        <h2>Data</h2>
+    <div class="data">
+        <h2>Información de Conversión...</h2>
+
+        <h3>
+            <span class="cript">
+                BTC 
+            </span>
+            - 
+            <span class="moneda">
+                USD
+            </span>
+        </h3>
+
+        <img src="" alt="">
+        <p>El precio es: <strong>$10000</strong></p>
+
     </div>
   
 </template>
--- 05_sass_http/src/components/Formulario.vue
+++ 05_sass_http/src/components/Formulario.vue
@@ -1,24 +1,24 @@
 <template>
     <div>
         
-        <form action="">
+        <form action="" class="form">
             <div>
                 <label for="">
                     Divisa: 
                 </label>
-                <select name="" id="" required>
+                <select name="" id="" required class="select">
                     <option value="" disabled selected>Elige una divisa..</option>
-                    <option value="USD">Dolar Estadunidense</option>
-                    <option value="MXN">Peso Mexicano</option>
-                    <option value="EUR">Euro</option>
-                    <option value="GBP">Libra Esterlina</option>
+                    <option value="USD">USD</option>
+                    <option value="EUR">EUR</option>
+                    <option value="GBP">GBP</option>
+                    <option value="JPY">JPY</option>
                 </select>
             </div>
             <div>
                 <label for="">
                     Criptomoneda:  
                 </label>
-                <select name="" id="" required>
+                <select name="" id="" required class="select">
                     <option value="" disabled selected>Elige una criptomoneda..</option>
                     <option value="BTC">Bitcoin</option>
                     <option value="ETH">Ethereum</option>
@@ -27,7 +27,7 @@
                 </select>
             </div>
             <div>
-                <button type="submit">
+                <button type="submit" class="buttons">
                     Cotizar
                 </button>
             </div>
