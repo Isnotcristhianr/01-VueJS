b42d0f87b1a6a8c3e550d734af9c714e82f8347c
Cristhian David Recalde <89264838+Isnotcristhianr@users.noreply.github.com>
2024-07-26 16:47:38 -0500

    feat: Update App.vue to use Vue Router

--- 07_spa/src/App.vue
+++ 07_spa/src/App.vue
@@ -1,6 +1,19 @@
+<script>
+import { RouterView } from 'vue-router';
+export default {
+  components: (
+    RouterView
+),
+}
+</script>
+
+
 <template>
   <div>
-    <h1>App view</h1>
+    <h1>Vue Router</h1>
+    <br>
+    <router-view />
+    
   </div>
 </template>
 
