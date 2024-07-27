0386d16f9cd029890f4fab9f8835ea8e6c0a2356
Cristhian David Recalde <89264838+Isnotcristhianr@users.noreply.github.com>
2024-07-26 23:40:08 -0500

    feat: Update Navbar.vue to use localized text for menu button

--- 07_spa/src/components/Navbar.vue
+++ 07_spa/src/components/Navbar.vue
@@ -16,18 +16,18 @@ const navigation = [
       <div class="relative flex h-16 items-center justify-between">
         <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
           <!-- Mobile menu button-->
-          <DisclosureButton
-            class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-            <span class="absolute -inset-0.5" />
-            <span class="sr-only">Open main menu</span>
-            <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
-            <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
-          </DisclosureButton>
+             <DisclosureButton
+               class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+               <span class="absolute -inset-0.5" />
+               <span class="sr-only">Abrir Menu</span>
+               <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+               <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
+             </DisclosureButton>
+           
         </div>
-        <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+        <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-center">
           <div class="flex flex-shrink-0 items-center">
-            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-              alt="Your Company" />
+            <img class="h-8 w-auto" src="/imgs/logo.jpg" alt="IsnotCristhianr" />
           </div>
           <div class="hidden sm:ml-6 sm:block">
             <div class="flex space-x-4">
@@ -37,16 +37,7 @@ const navigation = [
             </div>
           </div>
         </div>
-        <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-          <!-- busqueda -->
-          <div class="relative">
-            <input type="text" class="bg-gray-700 text-white px-3 py-1.5 rounded-md" placeholder="Search..." />
-            <button class="absolute right-0 top-0 mt-1 mr-1">
-              <BellIcon class="h-6 w-6 text-gray-400" />
-            </button>
-          </div>
 
-        </div>
       </div>
     </div>
 
