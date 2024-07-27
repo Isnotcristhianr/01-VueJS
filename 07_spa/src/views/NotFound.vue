8872fb2804a1163700df7dd73718ebb605864549
Cristhian David Recalde <89264838+Isnotcristhianr@users.noreply.github.com>
2024-07-26 19:39:00 -0500

    feat: Update NotFound.vue to use Tailwind CSS for styling

--- 07_spa/src/views/NotFound.vue
+++ 07_spa/src/views/NotFound.vue
@@ -1,12 +1,9 @@
 <template>
-    <div>
-        <h1>
-            Page Not Found
-        </h1>
-
-        <p>
-            The page you are looking for does not exist.
-        </p>
-
+   <!-- tailwind -->
+    <div class="flex items-center justify-center h-screen">
+        <div class="text-center">
+            <h1 class="text-4xl font-bold text-gray-800">404</h1>
+            <p class="text-gray-600">Page not found</p>
+        </div>
     </div>
 </template>
