330bbf88d3008187324a35939fb2a5a23cab84d6
Cristhian David Recalde <89264838+Isnotcristhianr@users.noreply.github.com>
2024-07-28 10:55:12 -0500

    feat: Update Navbar.vue to use computed properties for navigation links

--- 07_spa/src/components/Navbar.vue
+++ 07_spa/src/components/Navbar.vue
@@ -1,13 +1,17 @@
 <script setup>
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline'
+/* routerlocation */
+import { useRoute } from 'vue-router'
+import { computed } from 'vue'
+const route = useRoute()
 
-const navigation = [
-  { name: 'Home', href: '/', current: true },
-  { name: 'About', href: '/about', current: false },
-  { name: 'Gifs', href: '/gifs', current: false },
-  { name: 'Users', href: '/users', current: false },
-]
+const navigation = computed(() => [
+  { name: 'Home', href: '/', current: route.path === '/' },
+  { name: 'About', href: '/about', current: route.path === '/about' },
+  { name: 'Gifs', href: '/gifs', current: route.path === '/gifs' },
+  { name: 'Users', href: '/users', current: route.path === '/users' },
+])
 </script>
 
 <template>
@@ -16,24 +20,26 @@ const navigation = [
       <div class="relative flex h-16 items-center justify-between">
         <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
           <!-- Mobile menu button-->
-             <DisclosureButton
-               class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-               <span class="absolute -inset-0.5" />
-               <span class="sr-only">Abrir Menu</span>
-               <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
-               <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
-             </DisclosureButton>
-           
+          <DisclosureButton
+            class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+            <span class="absolute -inset-0.5" />
+            <span class="sr-only">Abrir Menu</span>
+            <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+            <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
+          </DisclosureButton>
+
         </div>
         <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-center">
           <div class="flex flex-shrink-0 items-center">
-            <img class="h-8 w-auto" src="/imgs/logo.jpg" alt="IsnotCristhianr" />
+            <router-link to="/" class="flex items-center">
+              <img class="h-8 w-auto" src="/imgs/logo.jpg" alt="IsnotCristhianr" />
+            </router-link>
           </div>
           <div class="hidden sm:ml-6 sm:block">
             <div class="flex space-x-4">
-              <a v-for="item in navigation" :key="item.name" :href="item.href"
-                :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'rounded-md px-3 py-2 text-sm font-medium']"
-                :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
+              <router-link v-for="item in navigation" :key="item.name" :to="item.href"
+                :class="[item.current ? 'bg-gray-500 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'px-3 py-2 rounded-md text-sm font-medium']"
+                :aria-current="item.current ? 'page' : undefined" active-class="active">{{ item.name }}</router-link>
             </div>
           </div>
         </div>
