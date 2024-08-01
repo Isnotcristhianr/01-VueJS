33956dbc7899bde9ae8d041298d287cb9a4cbc7b
Cristhian David Recalde <89264838+Isnotcristhianr@users.noreply.github.com>
2024-08-01 12:02:45 -0500

    feat: Add Inputs component for form input fields in HomeView lifecicle hooks

---
+++ 09_composition/src/components/Inputs.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+
+        <label for=""> Tec: #{{ num }} </label>
+        <br />
+        
+        <input
+        type="text"
+        v-model="formulario.name[num]"
+        name=""
+        id=""
+        placeholder="Ingresa tu Nombre"
+        />
+        <label for="">{{ formulario.name[num] }}</label>
+        <br />
+        
+        <select v-model="formulario.level[num]" name="nivel" id="">
+            <option value="0" disabled selected>Selecciona un nivel</option>
+            <option value="1">Nivel 1</option>
+            <option value="2">Nivel 2</option>
+            <option value="3">Nivel 3</option>
+        </select>
+        <label for="">{{ formulario.level[num] }}</label>
+    </div>
+</template>
+
+<script setup>
+import { defineProps, onUnmounted } from "vue";
+import { onMounted } from "vue";
+
+const { num, formulario } = defineProps(["num", "formulario"]);
+
+//life cicle hoocks
+onUnmounted(() => {
+    console.log("Componente desmontado");
+    formulario.name[num] = null;
+    formulario.level[num] = null;
+});
+</script>
+
+<style>
+
+</style>
--- 09_composition/src/views/HomeView.vue
+++ 09_composition/src/views/HomeView.vue
@@ -1,34 +1,41 @@
 <script setup>
-import { reactive, ref } from 'vue'
+import { reactive, ref } from "vue";
+import Inputs from "@/components/Inputs.vue";
 
-const msg = ref('Hola, Ref composition API')
-msg.value = 'Adios, Ref composition API'
+const msg = ref("Hola, Ref composition API");
+msg.value = "Adios, Ref composition API";
 
-const nombre = ref('Nombre')
+const nombre = ref("Nombre");
 
-const nivel = ref('0')
+const nivel = ref("0");
 
 //objetos reactivos
 const formulario = reactive({
   name: [],
   level: [],
-  num: 0
-})
+  num: 0,
+});
 </script>
 
 <template>
   <div>
     <h1>Refs</h1>
-    <hr>
+    <hr />
     {{ msg }}
 
-    <br>
-    <hr>
+    <br />
+    <hr />
 
     <!-- Formulario -->
     <h2>Refs</h2>
     <form action="">
-      <input type="text" v-model="nombre" name="" id="" placeholder="Ingresa tu Nombre">
+      <input
+        type="text"
+        v-model="nombre"
+        name=""
+        id=""
+        placeholder="Ingresa tu Nombre"
+      />
       <label for="">{{ nombre }}</label>
       <select v-model="nivel" name="nivel" id="">
         <option value="0" disabled selected>Selecciona un nivel</option>
@@ -39,36 +46,26 @@ const formulario = reactive({
       <label for="">{{ nivel }}</label>
     </form>
 
-    <hr>
+    <hr />
     <h2>Objetos reactivos</h2>
     <form action="">
       <label for="">
         Tegnologia:
-        <input type="number" name="" id="" min="1" max="5" v-model="formulario.num">
+        <input
+          type="number"
+          name=""
+          id=""
+          min="1"
+          max="5"
+          v-model="formulario.num"
+        />
         <label for="">{{ formulario.num }}</label>
       </label>
-      <br>
-
+      <br />
 
       <div v-for="num in formulario.num" :key="num">
-        <label for="">
-          Tec: #{{ num }}
-        </label>
-        <br>
-
-        <input type="text" v-model="formulario.name[num]" name="" id="" placeholder="Ingresa tu Nombre">
-        <label for="">{{ formulario.name[num] }}</label>
-        <br>
-
-        <select v-model="formulario.level[num]" name="nivel" id="">
-          <option value="0" disabled selected>Selecciona un nivel</option>
-          <option value="1">Nivel 1</option>
-          <option value="2">Nivel 2</option>
-          <option value="3">Nivel 3</option>
-        </select>
-        <label for="">{{ formulario.level[num] }}</label>
+        <inputs :num="num" :formulario="formulario" />
       </div>
     </form>
-
   </div>
 </template>
