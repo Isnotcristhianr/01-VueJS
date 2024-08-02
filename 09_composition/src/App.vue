9a8cf56f21ab3e94d8f4de17ab41eb15d9ef126a
Cristhian David Recalde <89264838+Isnotcristhianr@users.noreply.github.com>
2024-08-01 19:34:15 -0500

    feat: Add lazy loading for images in App.vue

--- 09_composition/src/App.vue
+++ 09_composition/src/App.vue
@@ -1,16 +1,22 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-<div>
-  <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav>
-  </header>
-  
-  <RouterView />
-</div>
+  <div>
+    <header>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </header>
+
+    <RouterView v-slot="{ Component }">
+      <transition name="fade" mode="out-in">
+        <keep-alive>
+          <component :is="Component" />
+        </keep-alive>
+      </transition>
+    </RouterView>
+  </div>
 </template>
