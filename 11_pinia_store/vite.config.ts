4ca68a939a47556fcbb7a4d25c94703552d55f3b
Cristhian David Recalde <89264838+Isnotcristhianr@users.noreply.github.com>
2024-08-21 23:10:56 -0500

    feat: Add lazy loading for images in Modal.vue and CardContent.vue components...proyect 12 blog clean with firebase

--- 11_pinia_store/vite.config.ts
+++ 11_pinia_store/vite.config.ts
@@ -2,7 +2,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import postcss from 'postcss';
 import tailwindcss from 'tailwindcss';   
 
 import autoprefixer from 'autoprefixer';
