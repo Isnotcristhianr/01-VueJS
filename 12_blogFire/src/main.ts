0738b67154eebe0b68c6a1a7b8365b35712d24d0
Cristhian David Recalde <89264838+Isnotcristhianr@users.noreply.github.com>
2024-08-22 22:56:00 -0500

    clean arquitecture, separate in carpets in base on respository dependencies

--- 12_blogFire/src/main.ts
+++ 12_blogFire/src/main.ts
@@ -1,10 +1,10 @@
-import "./assets/main.css";
+import "./presentation/assets/main.css";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
 import App from "./App.vue";
-import router from "./router";
+import router from "./presentation/router";
 
 import { VueFire, VueFireAuth } from "vuefire";
 import { firebaseApp } from "./config/firebaseConfig";
